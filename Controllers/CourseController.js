d540eb6ddaaeea1c68c1ea87cf25818368bf2bbf
Duy Ha <hacaoduy161@gmail.com>
2021-01-17 03:23:32 +0700

    mẻge

--- Controllers/CourseController.js
+++ Controllers/CourseController.js
@@ -6,54 +6,54 @@ const CourseDetailService = require('../Service/CourseDetailService')
 
 const Offset = process.env.OFFSET
 
-const ListCourse4Index = async function(req,res,next){
+const ListCourse4Index = async function(req, res, next) {
     let listAll = []
     let listMobile = []
     let listWebsite = []
 
     listAll = await CourseService.all('')
-    if (listAll !== null && listAll.length > 3){
-        listAll = listAll.slice(0,3)
-    }    
+    if (listAll !== null && listAll.length > 3) {
+        listAll = listAll.slice(0, 3)
+    }
     let mobileCourseId = await CourseService.getCourseTypeId(process.env.MOBILE)
-    if (mobileCourseId !== null && mobileCourseId !== undefined){
+    if (mobileCourseId !== null && mobileCourseId !== undefined) {
         mobileCourseId = mobileCourseId[0].Id
-        listMobile = await CourseService.getCourseByCourseTypeId(mobileCourseId,'');
-        if (listMobile !== null && listMobile.length > 3){
-            listMobile = listMobile.slice(0,3)
+        listMobile = await CourseService.getCourseByCourseTypeId(mobileCourseId, '');
+        if (listMobile !== null && listMobile.length > 3) {
+            listMobile = listMobile.slice(0, 3)
         }
     }
     let websiteCourseId = await CourseService.getCourseTypeId(process.env.WEBSITE)
-    if (websiteCourseId !== null && websiteCourseId !== undefined){
+    if (websiteCourseId !== null && websiteCourseId !== undefined) {
         websiteCourseId = websiteCourseId[0].Id;
-        listWebsite = await CourseService.getCourseByCourseTypeId(websiteCourseId,'');
-        if (listWebsite !== null && listWebsite.length > 3){
-            listWebsite = listWebsite.slice(0,3)
+        listWebsite = await CourseService.getCourseByCourseTypeId(websiteCourseId, '');
+        if (listWebsite !== null && listWebsite.length > 3) {
+            listWebsite = listWebsite.slice(0, 3)
         }
     }
 
-    let mostViewedCourses  = await CourseService.get12MostViewedCourses()
+    let mostViewedCourses = await CourseService.get12MostViewedCourses()
     console.log(mostViewedCourses.length)
     let latestCourses = await CourseService.get12LatestCourses()
     console.log(latestCourses.length)
-    res.render('./index',{
+    res.render('./index', {
         title: 'Trang chủ',
-        ListAll : listAll,
-        ListWebsite : listWebsite,
-        ListMobile : listMobile,
-        MostViewedCourses : mostViewedCourses,
-        LatestCourses : latestCourses,
+        ListAll: listAll,
+        ListWebsite: listWebsite,
+        ListMobile: listMobile,
+        MostViewedCourses: mostViewedCourses,
+        LatestCourses: latestCourses,
     })
 }
 
-function CreatePageArray(page){
+function CreatePageArray(page) {
     var data = []
-    for (let index = 1; index < page+1; index++){
+    for (let index = 1; index < page + 1; index++) {
         data.push(index)
     }
     return data
 }
-const ListCourse = async function(req,res,next) {
+const ListCourse = async function(req, res, next) {
     let listData = [];
     let IsFilter = false;
     let IsSorting = false;
@@ -67,61 +67,56 @@ const ListCourse = async function(req,res,next) {
     let priceSorting = req.query.PriceSorting;
     let rateSorting = req.query.RateSorting;
     let viewSorting = req.query.ViewSorting;
-    if (priceSorting !== undefined){
+    if (priceSorting !== undefined) {
         IsSorting = true;
-        option = 'Order by Price '+priceSorting
-    }
-    else if (rateSorting !== undefined){
+        option = 'Order by Price ' + priceSorting
+    } else if (rateSorting !== undefined) {
         IsSorting = true;
-        option = 'Order by Rate '+ rateSorting
-    }
-    else if (viewSorting !== undefined){
+        option = 'Order by Rate ' + rateSorting
+    } else if (viewSorting !== undefined) {
         IsSorting = true;
-        option = 'Order by View '+ viewSorting
+        option = 'Order by View ' + viewSorting
     }
 
-    if (courseTypeId === undefined){
+    if (courseTypeId === undefined) {
         listData = await CourseService.all(option);
-        if (listData !== null && listData.length > 0){
+        if (listData !== null && listData.length > 0) {
             let lenAll = listData.length
-            pagesAll = CreatePageArray(lenAll%Offset == 0 ? Math.floor(lenAll/Offset) : Math.floor(lenAll/Offset) + 1)
-            if (page*Offset-1 < listData.length){
-                listData = listData.slice((page-1)*Offset,page*Offset)
-            }
-            else {
-                listData = listData.slice((page-1)*Offset,listData.length)
+            pagesAll = CreatePageArray(lenAll % Offset == 0 ? Math.floor(lenAll / Offset) : Math.floor(lenAll / Offset) + 1)
+            if (page * Offset - 1 < listData.length) {
+                listData = listData.slice((page - 1) * Offset, page * Offset)
+            } else {
+                listData = listData.slice((page - 1) * Offset, listData.length)
             }
         }
-    }
-    else{
-        listData = await  CourseService.getCourseByCourseTypeId(courseTypeId,option);
-        if (listData !== null && listData.length  > 0){
+    } else {
+        listData = await CourseService.getCourseByCourseTypeId(courseTypeId, option);
+        if (listData !== null && listData.length > 0) {
             IsFilter = true;
             courseType = courseTypeId
             let lenAll = listData.length
-            pagesAll = CreatePageArray(lenAll%Offset == 0 ? Math.floor(lenAll/Offset) : Math.floor(lenAll/Offset) + 1)
-            if (page*Offset-1 < listData.length){
-                listData = listData.slice((page-1)*Offset,page*Offset)
-            }
-            else {
-                listData = listData.slice((page-1)*Offset,listData.length)
+            pagesAll = CreatePageArray(lenAll % Offset == 0 ? Math.floor(lenAll / Offset) : Math.floor(lenAll / Offset) + 1)
+            if (page * Offset - 1 < listData.length) {
+                listData = listData.slice((page - 1) * Offset, page * Offset)
+            } else {
+                listData = listData.slice((page - 1) * Offset, listData.length)
             }
         }
     }
-    res.render('./course/course',{
-        title:'Khóa học',
-        ListAll : listData,
-        PagesAll : pagesAll,
-        IsFilter : IsFilter,
-        CourseType : courseType,
-        IsSorting : IsSorting,
-        PriceSorting : priceSorting,
-        RateSorting : rateSorting,
-        ViewSorting : viewSorting,
+    res.render('./course/course', {
+        title: 'Khóa học',
+        ListAll: listData,
+        PagesAll: pagesAll,
+        IsFilter: IsFilter,
+        CourseType: courseType,
+        IsSorting: IsSorting,
+        PriceSorting: priceSorting,
+        RateSorting: rateSorting,
+        ViewSorting: viewSorting,
     })
 }
 
-const DetailCourse = async function(req,res,next){
+const DetailCourse = async function(req, res, next) {
     let Id = req.query.ID;
     console.log(Id)
     let user = 'Default';
@@ -130,27 +125,27 @@ const DetailCourse = async function(req,res,next){
     let rates = [];
     let detailCourse = await CourseService.single(Id)
     console.log(detailCourse)
-    if (detailCourse !== null){
-        var update = await CourseService.updateViews(detailCourse.ID,detailCourse.Views + 1)
-        if (detailCourse.UserId !== null){
+    if (detailCourse !== null) {
+        var update = await CourseService.updateViews(detailCourse.ID, detailCourse.Views + 1)
+        if (detailCourse.UserId !== null) {
             user = await UserService.singleById(detailCourse.UserId)
         }
-        if (detailCourse.CurrentStudents === detailCourse.MaxStudents){
+        if (detailCourse.CurrentStudents === detailCourse.MaxStudents) {
             isFull = true
         }
-        rates = await CourseDetailService.allRateByCourseID(Id)      
+        rates = await CourseDetailService.allRateByCourseID(Id)
     }
-    if (req.session.Account !== null && req.session.Account !== undefined){
-        isJoin = await CourseDetailService.checkJoin(req.session.Account.ID,detailCourse.ID)  
+    if (req.session.Account !== null && req.session.Account !== undefined) {
+        isJoin = await CourseDetailService.checkJoin(req.session.Account.ID, detailCourse.ID)
     }
     console.log(isJoin)
-    res.render('./course/detailCourse',{
-        title:'Chi tiết khóa học',
-        data : detailCourse,
-        User : user,
-        IsJoin : isJoin,
-        IsFull : isFull,
-        Rates : rates
+    res.render('./course/detailCourse', {
+        title: 'Chi tiết khóa học',
+        data: detailCourse,
+        User: user,
+        IsJoin: isJoin,
+        IsFull: isFull,
+        Rates: rates
     })
 }
 
